6ef36fc2374b68092190b698dbeff7abc0e0730b
Brady Wied <brady@bswtechconsulting.com>
2015-10-20 09:54:03 -0600

    Outline scenarios

--- spec/mri/spring_spec.rb
+++ spec/mri/spring_spec.rb
@@ -1,3 +1,17 @@
 describe SpringCommandsOrspec do
-  pending 'write this'
+  context 'spring not running' do
+    pending 'write this'
+  end
+
+  context 'spring already running' do
+    pending 'write this'
+  end
+
+  context 'Rakefile/pattern changed' do
+    pending 'write this'
+  end
+
+  context 'test changed' do
+    pending 'write this'
+  end
 end
