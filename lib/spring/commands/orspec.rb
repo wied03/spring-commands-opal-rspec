0496c4df40af3351c6fbe2e361b1601c2955b259
Brady Wied <brady@bswtechconsulting.com>
2015-10-21 18:51:40 -0600

    Remove commented out code, ensure we don't override our PI
    If we have subsequent executions where the server is already started, we might overwrite this

--- lib/spring/commands/orspec.rb
+++ lib/spring/commands/orspec.rb
@@ -2,93 +2,12 @@ require 'spring/application'
 require 'spring/commands'
 require 'opal/rspec/rake_task'
 
-# module Opal
-#   module RSpec
-#     class ApplicationManager < ::Spring::ApplicationManager
-#       def initialize(app_env)
-#         super
-#         @app_env = 'opal_rspec'
-#       end
-#
-#       def start_child(preload = false)
-#         sprockets_env = Opal::RSpec::SprocketsEnvironment.new
-#         app = Opal::Server.new(sprockets: sprockets_env) { |s|
-#           s.main = 'opal/rspec/sprockets_runner'
-#           s.debug = false
-#
-#           sprockets_env.spec_pattern = ENV['PATTERN'] || (::Rails.application.config.opal.spec_location+'/**/*_spec{,.js}.{rb,opal}')
-#           ::Rails.application.assets.paths.each { |p| s.append_path p }
-#           sprockets_env.add_spec_paths_to_sprockets
-#         }
-#         @pid = fork {
-#           Rack::Server.start(
-#               :app => app,
-#               :Port => PORT,
-#               :AccessLog => [],
-#               :Logger => WEBrick::Log.new("/dev/null"),
-#           )
-#         }
-#         puts "server pid is #{@pid}"
-#         wait_for_server
-#       end
-#
-#       def wait_for_server
-#         # avoid retryable dependency
-#         tries = 0
-#         up = false
-#         uri = URI(URL)
-#         while tries < 4 && !up
-#           tries += 1
-#           sleep 0.1
-#           begin
-#             # Using TCPSocket, not net/http open because executing the HTTP GET / will incur a decent delay just to check if the server is up
-#             # in order to better communicate to the user what is going on, save the actual HTTP request for the phantom/node run
-#             # the only objective here is to see if the Rack server has started
-#             socket = TCPSocket.new uri.hostname, uri.port
-#             up = true
-#             socket.close
-#           rescue Errno::ECONNREFUSED
-#             # server not up yet
-#           end
-#         end
-#         raise 'Tried 4 times to contact Rack server and not up!' unless up
-#       end
-#     end
-#
-#     class Server < ::Spring::Server
-#       def initialize(env = ::Spring::Env.new)
-#         super
-#         @applications = Hash.new { |h, k| h[k] = Opal::RSpec::ApplicationManager.new(k) }
-#       end
-#     end
-#
-#     class Client < Spring::Client::Run
-#       def boot_server
-#         env.socket_path.unlink if env.socket_path.exist?
-#
-#         pid = Process.spawn(
-#             gem_env,
-#             "ruby",
-#             "-e", "gem 'spring', '#{Spring::VERSION}'; require 'spring-commands-orspec'; Opal::RSpec::Server.boot"
-#         )
-#
-#         until env.socket_path.exist?
-#           _, status = Process.waitpid2(pid, Process::WNOHANG)
-#           exit status.exitstatus if status
-#           sleep 0.1
-#         end
-#       end
-#     end
-#   end
-# end
-
 module Spring
   class Application
     def setup(command_wrapper)
       setup = if command_wrapper.command.is_a?(Spring::Commands::Orspec)
                 pid = command_wrapper.command.setup
-                puts "spring app, got pid #{pid} back"
-                @opal_rspec_pid = pid
+                @opal_rspec_pid ||= pid
               else
                 command_wrapper.setup
               end
